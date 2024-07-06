68559b36586db8a4681e02f399f4487b4edf0782
Tin Lai <oscar@tinyiu.com>
2024-07-06 12:50:51 +1000

    work in progress of using one thread per request
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
+use std::io;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 use std::process::exit;
 use std::str;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::Duration;
+use std::{error::Error, fmt};
 
 use clap::Parser;
 
@@ -32,6 +35,10 @@ struct Args {
     #[arg(short, long, default_value_t = 1)]
     count: u8,
 
+    /// Number of seconds before timing out the socket wait
+    #[arg(short, long, default_value_t = 5)]
+    timeout: u64,
+
     /// Pre-shared key
     #[arg(short, long, default_value = "HA12")]
     preshared_key: String,
@@ -45,129 +52,180 @@ macro_rules! println_if_verbose {
     };
 }
 
+#[derive(Debug)]
+struct TimeoutError {
+    message: String,
+    addr: Option<SocketAddr>,
+}
+
+impl TimeoutError {
+    fn to_message(&self) -> String {
+        format!(
+            "{}: {}",
+            self.message,
+            self.addr
+                .map_or("[..]".to_owned(), |addr| { addr.to_string() })
+        )
+    }
+}
+
+// impl From<io::Error> for TimeoutError {
+//     fn from(error: io::Error) -> Self {
+//         TimeoutError {
+//             // kind: String::from("parse"),
+//             message: error.to_string(),
+//         }
+//     }
+// }
+
 fn loop_process_socket<T>(
     socket: &UdpSocket,
     mut on_message_functor: impl FnMut(&[u8], SocketAddr) -> Option<T>,
-) -> T {
+) -> Result<T, TimeoutError> {
     // loop untils some value is returned by the functor
     let mut buf = [0u8; 65535];
     loop {
         match socket.recv_from(&mut buf) {
-            Ok((n, from)) => {
-                if n > 0 {
-                    match on_message_functor(&buf[..n], from) {
-                        Some(v) => return v,
-                        None => {}
-                    }
-                }
+            Ok((n, from)) if n > 0 => match on_message_functor(&buf[..n], from) {
+                Some(v) => return Ok(v),
+                None => {}
+            },
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                return Err(TimeoutError {
+                    message: format!("Timeout reached",),
+                    addr: socket.local_addr().ok(),
+                });
             }
-            Err(_) => {}
-        }
+            _ => {}
+        };
     }
 }
 
-fn wait_for_peers_check_psk(fsrc: &UdpSocket, args: &Args) -> SocketAddr {
+fn wait_for_peers_check_psk(fsrc: &UdpSocket, args: &Args) -> Result<SocketAddr, TimeoutError> {
     // function that verifies PSK
     let udp = fsrc.local_addr().unwrap().port();
     println_if_verbose!(args.verbose, "> Waiting for PSK @UDP {}", udp);
 
-    let result_addr: SocketAddr = loop_process_socket(&fsrc, |message, from| {
-        match str::from_utf8(&message) {
-            Ok(v) => {
-                if v.trim() == args.preshared_key {
-                    println_if_verbose!(args.verbose, "> Got peer info for {}", from);
-                    return Some(from);
-                }
-            }
-            Err(_) => {}
+    loop_process_socket(&fsrc, |message, from| {
+        let Ok(v) = str::from_utf8(&message) else {
+            return None;
         };
-        println_if_verbose!(args.verbose, ">> Invalid key @UDP {}, from {}", udp, from);
+        if v.trim() == args.preshared_key {
+            println_if_verbose!(args.verbose, "> Got peer info for {from}");
+            return Some(from);
+        }
+        println_if_verbose!(args.verbose, ">> Invalid key @UDP {udp}, from {from}");
         None
-    });
-    result_addr
+    })
 }
 
+struct Peer<'a> {
+    socket: &'a UdpSocket,
+    addr: Mutex<SocketAddr>,
+}
+// struct Peer<'a> {
+//     socket: &'a UdpSocket,
+//     addr: Arc<Mutex<SocketAddr>>,
+// }
+
 // Worker function to handle UDP packet forwarding
-fn worker(
-    fsrc: &UdpSocket,
-    fdst: &UdpSocket,
-    ssrc: Arc<Mutex<SocketAddr>>,
-    sdst: Arc<Mutex<SocketAddr>>,
-    args: &Args,
-) {
-    fn _verbose_print_recieved_data(
-        message: &[u8],
-        from: SocketAddr,
-        ssrc: SocketAddr,
-        sdst: SocketAddr,
-    ) {
-        let string = match str::from_utf8(&message) {
-            Ok(v) => v,
-            Err(_) => &format!("[some bytes of length {}]", message.len()),
-        };
-        println!("> {} => {}: {} | => {}", from, ssrc, string.trim(), sdst)
-    }
+// fn relay_worker(
+//     src: &Peer,
+//     dst: &Peer,
 
-    for i in 0..args.count {
-        loop_process_socket(&fsrc, |message, from| {
-            let mut sender = ssrc.lock().unwrap();
-            *sender = from;
+//     args: &Args,
+// ) -> Result<(), TimeoutError> {
+//     let a = relay_worker__(fsrc, fdst, ssrc, sdst, args);
+//     println!("exiting... {:?}", fsrc);
+//     a
+// }
+fn relay_worker(src: &Peer, dst: &Peer, args: &Args) -> Result<(), TimeoutError> {
+    // helper function to print output
+    let _verbose_print_recieved_data = |message: &[u8], from: SocketAddr| {
+        if !args.verbose {
+            return;
+        }
+        let message = str::from_utf8(&message)
+            .and_then(|m| Ok(m.to_owned()))
+            .unwrap_or(format!("[some bytes of length {}]", message.len()));
+        println!(
+            "> {} => {}: {} | => {}",
+            from,
+            src.addr.lock().unwrap(),
+            message.trim(),
+            dst.addr.lock().unwrap()
+        );
+    };
 
-            if args.verbose {
-                _verbose_print_recieved_data(message, from, *sender, *sdst.lock().unwrap());
-                println!(">> Current pre process {}", i)
-            }
-            drop(sender);
-            // let a = sdst.lock().unwrap();
+    for _ in 0..args.count {
+        loop_process_socket(&src.socket, |message, from| {
+            // update the peer source address as the actual current from's port
+            *src.addr.lock().unwrap() = from;
 
-            fdst.send_to(&message, *sdst.lock().unwrap()).unwrap();
+            _verbose_print_recieved_data(message, from);
+
+            dst.socket
+                .send_to(&message, *dst.addr.lock().unwrap())
+                .unwrap();
             Some(()) // break every time
-        })
+        })?
     }
+    println_if_verbose!(args.verbose, ">> Locking adress");
 
-    // we will now fix it
-    let fixed_src_addr: SocketAddr = ssrc.lock().unwrap().clone();
-    let fixed_dst_addr: SocketAddr = sdst.lock().unwrap().clone();
+    // we will now fix the address
+    let fixed_src_addr: SocketAddr = src.addr.lock().unwrap().clone();
+    let fixed_dst_addr: SocketAddr = dst.addr.lock().unwrap().clone();
 
-    loop_process_socket(&fsrc, |message, from| {
+    loop_process_socket(&src.socket, |message, from| {
         if !args.no_reject_unknown_sender && fixed_src_addr != from {
             println_if_verbose!(args.verbose, "> Rejected unknown peer {}", from);
             return None;
         }
 
-        if args.verbose {
-            _verbose_print_recieved_data(message, from, fixed_src_addr, fixed_dst_addr);
-        }
-        fdst.send_to(&message, fixed_dst_addr).unwrap();
+        _verbose_print_recieved_data(message, from);
+        dst.socket.send_to(&message, fixed_dst_addr).unwrap();
         None
     })
 }
 
-fn bind_or_exit(ip: Ipv4Addr, port: u16) -> UdpSocket {
-    match UdpSocket::bind((ip, port)) {
-        Ok(socket) => return socket,
-        Err(_) => {
-            eprintln!("Failed to bind {}:{}", ip, port);
-            exit(1);
-        }
-    }
+fn bind_socket(ip: Ipv4Addr, port: u16, args: &Args) -> Result<UdpSocket, io::Error> {
+    UdpSocket::bind((ip, port)).and_then(|socket| {
+        socket
+            .set_read_timeout(Some(Duration::new(args.timeout, 0)))
+            .ok();
+        Ok(socket)
+    })
 }
 
 fn main() {
     let args = Args::parse();
 
     // Create UDP sockets for port A and port B
-    let udp_a = bind_or_exit(args.bind_ip, args.port_a);
-    let udp_b = bind_or_exit(args.bind_ip, args.port_b);
+    let udp_a = bind_socket(args.bind_ip, args.port_a, &args).expect("cannot binds socket");
+    let udp_b = bind_socket(args.bind_ip, args.port_b, &args).expect("cannot binds socket");
 
-    // verify pre-shared key
-    let peers = thread::scope(|scope| {
-        let peer_a = scope.spawn(|| wait_for_peers_check_psk(&udp_a, &args));
-        let peer_b = scope.spawn(|| wait_for_peers_check_psk(&udp_b, &args));
-        let peer_a = peer_a.join().unwrap();
-        let peer_b = peer_b.join().unwrap();
-        return (peer_a, peer_b);
-    });
+    // // verify pre-shared key
+    // let peers = thread::scope(|scope| {
+    //     let peer_a = scope.spawn(|| wait_for_peers_check_psk(&udp_a, &args));
+    //     let peer_b = scope.spawn(|| wait_for_peers_check_psk(&udp_b, &args));
+
+    //     // let peer_a = peer_a.join().unwrap().ok().unwrap();
+    //     let peer_a = match peer_a.join().unwrap() {
+    //         Ok(v) => v,
+    //         Err(e) => {
+    //             print!("{:?}", e);
+    //             panic!("i------------");
+    //         }
+    //     };
+
+    //     let peer_b = peer_b.join().unwrap().ok().unwrap();
+    //     return (peer_a, peer_b);
+    // });
+
+    let peers = (
+        SocketAddr::new(IpAddr::V4(args.bind_ip), args.port_a),
+        SocketAddr::new(IpAddr::V4(args.bind_ip), args.port_b),
+    );
 
     // // Socket addresses for binding
     // let sock_a: SocketAddr = SocketAddr::new(IpAddr::V4(args.bind_ip), args.port_a);
@@ -178,16 +236,72 @@ fn main() {
 
     // Create threads for bidirectional UDP relay
     thread::scope(|scope| {
-        let peer_a = Arc::new(Mutex::new(peers.0));
-        let peer_b = Arc::new(Mutex::new(peers.1));
-        let peer_a_clone: Arc<Mutex<SocketAddr>> = peer_a.clone();
-        let peer_b_clone: Arc<Mutex<SocketAddr>> = peer_b.clone();
+        let join_thread_handle = |handle: thread::ScopedJoinHandle<Result<(), TimeoutError>>| {
+            if let Err(e) = handle.join().unwrap() {
+                println_if_verbose!(args.verbose, "{}: ", e.to_message());
+            }
+        };
 
-        let udp_a = &udp_a;
-        let udp_b = &udp_b;
+        let mut handles = Vec::new();
+
         let args = &args;
+        for _ in 1..5 {
+            let peer_a = Arc::new(Peer {
+                socket: &udp_a,
+                addr: Mutex::new(peers.0),
+            });
+            let peer_b = Arc::new(Peer {
+                socket: &udp_b,
+                addr: Mutex::new(peers.1),
+            });
+            let peer_a_clone = peer_a.clone();
+            let peer_b_clone = peer_b.clone();
+            handles.push(scope.spawn(move || relay_worker(&peer_a_clone, &peer_b_clone, args)));
+            handles.push(scope.spawn(move || relay_worker(&peer_b, &peer_a, args)));
+        }
 
-        scope.spawn(move || worker(&udp_a, &udp_b, peer_a_clone, peer_b_clone, &args));
-        scope.spawn(move || worker(&udp_b, &udp_a, peer_b, peer_a, &args));
+        while handles.len() > 0 {
+            // join_thread_handle(handles.remove(0));
+
+            if let Err(e) = handles.remove(0).join().unwrap() {
+                println_if_verbose!(args.verbose, "{}: ", e.to_message());
+            }
+        }
+        // {
+        //     let peer_a = Arc::new(Peer {
+        //         socket: &udp_a,
+        //         addr: Mutex::new(peers.0),
+        //     });
+        //     let peer_b = Arc::new(Peer {
+        //         socket: &udp_b,
+        //         addr: Mutex::new(peers.1),
+        //     });
+        //     let peer_a_clone = peer_a.clone();
+        //     let peer_b_clone = peer_b.clone();
+        //     scope.spawn(move || relay_worker(&peer_a_clone, &peer_b_clone, args));
+        //     scope.spawn(move || relay_worker(&peer_b, &peer_a, args));
+        // }
+        // {
+        //     let peer_a = Arc::new(Peer {
+        //         socket: &udp_a,
+        //         addr: Mutex::new(peers.0),
+        //     });
+        //     let peer_b = Arc::new(Peer {
+        //         socket: &udp_b,
+        //         addr: Mutex::new(peers.1),
+        //     });
+        //     let peer_a_clone = peer_a.clone();
+        //     let peer_b_clone = peer_b.clone();
+        //     scope.spawn(move || relay_worker(&peer_a_clone, &peer_b_clone, args));
+        //     scope.spawn(move || relay_worker(&peer_b, &peer_a, args));
+        // }
+
+        // let peer_a_clone = peer_a.clone();
+        // let peer_b_clone = peer_b.clone();
+        // scope.spawn(move || relay_worker(&udp_a, &udp_b, peer_a_clone, peer_b_clone, &args));
+        // let peer_a_clone = peer_a.clone();
+        // let peer_b_clone = peer_b.clone();
+        // scope.spawn(move || relay_worker(&udp_a, &udp_b, peer_a_clone, peer_b_clone, &args));
     });
+    println!("end");
 }
